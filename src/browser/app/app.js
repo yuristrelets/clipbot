0dac02afe6420571baa789991108e8612c959aaa
yuristrelets <yuristrelets@gmail.com>
2016-09-18 01:41:03 +0300

    replace app with react

--- src/browser/app/app.js
+++ src/browser/app/app.js
@@ -1,4 +1,4 @@
-import { ipcRenderer } from 'electron';
+/*import { ipcRenderer } from 'electron';
 import styles from './app.less';
 
 const $list = document.getElementById('list');
@@ -11,12 +11,42 @@ ipcRenderer.on('clipboard.list.updated', function(event, list) {
 
 function renderList(list, element) {
   var html = list.map(function(item, index) {
-    return '<div>' + item.previewText + '</div>';
+    return '<div>' + item.preview + '</div>';
   });
 
   element.innerHTML = html.join('');
 }
 
-// ipcRenderer.on('clipboardFormats', function(event, formats) {
-//   console.log('-->', formats);
-// });
+ ipcRenderer.on('clipboardFormats', function(event, formats) {
+   console.log('-->', formats);
+ });*/
+
+import React from 'react';
+import { render } from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import './app.less';
+
+const rootReducer = combineReducers({
+  data: (state = null) => {
+    return state;
+  }
+});
+
+const store = createStore(rootReducer);
+
+class App extends React.Component {
+  render() {
+    return (
+      <span>application</span>
+    );
+  }
+}
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
